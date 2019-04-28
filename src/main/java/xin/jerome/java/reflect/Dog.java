1c7bb27b276bd2e16c3c861a10203cd2cf87d0e7
JeromeZhu <JeromeZhu666@gmail.com>
2019-04-28 16:55:21 +0800

    添加测试反射内容

---
+++ src/main/java/xin/jerome/java/reflect/Dog.java
@@ -0,0 +1,20 @@
+package xin.jerome.java.reflect;
+
+/**
+ * 狗对象
+ *
+ * @author Jerome Zhu
+ * @since 2019.04.28 16:24
+ */
+public class Dog {
+    private String name;
+
+    public String sayName() {
+        return String.format("my name is %s", name);
+    }
+
+    private String say(String word) {
+        return "Hello " + word;
+    }
+
+}
